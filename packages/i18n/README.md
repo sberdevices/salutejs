1154b3135ab3a9496ad8759de06320f66f2adda6
Anton Vinogradov <winogradovaa@gmail.com>
2021-03-26 01:51:47 +0300

    feat: add i18n package

---
+++ packages/i18n/README.md
@@ -0,0 +1,107 @@
+# I18N
+
+Интерфейс для адаптации текста с динамическими параметрами и плюрализацией в рамках персонажей семейства виртуальных ассистентов Салют.
+
+## Создание словарей
+
+Файлы с адаптацией лежат рядом с кодом, к которому они логически относятся.
+
+```
+src/handlers/mainPage
+├── mainPage.i18n
+│   ├── sber.ts — словарь для персонажа Сбер
+│   ├── joy.ts — словарь для персонажа Джой
+│   ├── athena.ts — словарь для персонажа Афина
+│   └── index.ts — карта персонажей
+```
+
+Файл словаря — модуль, в котором лежит кейсет для одного языка с парами `{ ключ, перевод }`:
+
+``` ts
+// mainPage/mainPage.i18n/sber.ts
+export const sber = {
+    Пока: 'Bye',
+    Привет: 'Example',
+};
+```
+
+Все словари должны быть объявлены в карте персонажей. Картой оперирует модуль i18n.
+```ts
+// mainPage/mainPage.i18n/index.ts
+export * from './sber';
+export * from './joy';
+export * from './athena';
+```
+
+
+## Использование
+
+``` ts
+// mainPage/mainPage.ts
+import i18n from '@salutejs/i18n';
+
+import * as keyset from './mainPage.i18n';
+
+const greeting = i18n(keyset);
+greeting('Привет');
+```
+
+### Параметризация
+
+Параметры объявляются в синтаксисе схожем с параметрами для [template strings](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/template_strings).
+
+``` ts
+// mainPage/mainPage.i18n/sber.ts
+export const sber = {
+    'Добрый день, {name}!': 'Добрый день, {name}!',
+}
+```
+
+``` ts
+// mainPage/mainPage.i18n/joy.ts
+export const joy = {
+    'Добрый день, {name}!': 'Привет, {name}!',
+}
+```
+
+``` ts
+// mainPage/mainPage.ts
+import i18n from '@salutejs/i18n';
+
+import * as keyset from './mainPage.i18n';
+
+const greeting = i18n(keyset);
+
+greeting('Добрый день, {name}!', {
+    name: 'Костя',
+});
+```
+
+### Плюрализация
+
+Для выражения плюрализация существует специальный параметр `count`. Который соотносится с вариантами написания ключа через вложенные параметры: `many`, `some`, `one`, `none`.
+
+``` ts
+// mainPage/mainPage.i18n/sber.ts
+export const sber = {
+    '{count} операций': {
+        many: '{count} операция',
+        none: 'нет операций',
+        one: '{count} операция',
+        some: '{count} операции',
+    }
+}
+```
+
+```ts
+// mainPage/mainPage.ts
+import i18n from '@salutejs/i18n';
+
+import * as keyset from './mainPage.i18n';
+
+const transactions = i18n(keyset);
+
+transactions('{count} операций', {
+    count: 2,
+});
+```
---
+++ packages/i18n/src/index.spec.ts
@@ -0,0 +1,92 @@
+import { CharacterId } from '@salutejs/types';
+
+import { i18n, setI18nCharacter } from '.';
+
+describe('i18n', () => {
+    const keysetDict = {
+        joy: {
+            '{count} яблок': {
+                many: '{count} apples',
+                none: 'no apples',
+                one: '{count} apple',
+                some: '{count} apples',
+            },
+            Пример: 'Example',
+            'ул. {street} неподалеку': '{street} st. nearby',
+        },
+        sber: {
+            '{count} яблок у {number} студентов': {
+                many: '{count} яблок у {number} студентов',
+                none: 'нет яблок',
+                one: '{count} яблоко у {number} студентов',
+                some: '{count} яблока у {number} студентов',
+            },
+            Пример: 'Пример',
+            'ул. {street} неподалеку': 'ул. {street} совсем рядом',
+            'Только СБЕР': 'Только СБЕР',
+        },
+    };
+
+    describe('sber (default)', () => {
+        it('should handle pluralization', () => {
+            const adapterI18n = i18n(keysetDict);
+
+            expect(adapterI18n('{count} яблок у {number} студентов', { count: 0, number: 42 })).toEqual('нет яблок');
+            expect(adapterI18n('{count} яблок у {number} студентов', { count: 1, number: 42 })).toEqual(
+                '1 яблоко у 42 студентов',
+            );
+            expect(adapterI18n('{count} яблок у {number} студентов', { count: 3, number: 42 })).toEqual(
+                '3 яблока у 42 студентов',
+            );
+            expect(adapterI18n('{count} яблок у {number} студентов', { count: 5, number: 42 })).toEqual(
+                '5 яблок у 42 студентов',
+            );
+            expect(adapterI18n('{count} яблок у {number} студентов', { count: 22, number: 42 })).toEqual(
+                '22 яблока у 42 студентов',
+            );
+            expect(adapterI18n('{count} яблок у {number} студентов', { count: 25, number: 42 })).toEqual(
+                '25 яблок у 42 студентов',
+            );
+            expect(adapterI18n('{count} яблок у {number} студентов', { count: 1001, number: 42 })).toEqual(
+                '1001 яблоко у 42 студентов',
+            );
+        });
+    });
+
+    describe('joy', () => {
+        beforeEach(() => {
+            setI18nCharacter(CharacterId.joy);
+        });
+
+        it('should get simple text', () => {
+            expect(i18n(keysetDict)('Пример')).toEqual('Example');
+        });
+
+        it('should substitute params', () => {
+            expect(i18n(keysetDict)('ул. {street} неподалеку', { street: 'Тверская' })).toEqual('Тверская st. nearby');
+        });
+
+        it('should handle pluralization', () => {
+            const adapterI18n = i18n(keysetDict);
+
+            expect(adapterI18n('{count} яблок', { count: 0 })).toEqual('no apples');
+            expect(adapterI18n('{count} яблок', { count: 1 })).toEqual('1 apple');
+            expect(adapterI18n('{count} яблок', { count: 2 })).toEqual('2 apples');
+            expect(adapterI18n('{count} яблок', { count: 1001 })).toEqual('1001 apple');
+        });
+    });
+
+    describe('missing translate', () => {
+        it('missing in athena', () => {
+            setI18nCharacter(CharacterId.athena);
+
+            expect(i18n(keysetDict)('Только СБЕР')).toEqual('Только СБЕР');
+        });
+
+        it('missing in everywhere', () => {
+            setI18nCharacter(CharacterId.athena);
+
+            expect(i18n(keysetDict)('Нигде нет перевода')).toEqual('Нигде нет перевода');
+        });
+    });
+});
