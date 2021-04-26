17f618b3ac22c5d3a7f91aa138bbae2a57b81ac4
Anton Vinogradov <winogradovaa@gmail.com>
2021-04-26 11:32:07 +0300

    chore: merge payments to scenario

--- packages/payments/src/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-    createInvoice,
-    findInvoiceById,
-    findInvoiceByServiceIdOrderId,
-    compliteInvoice,
-    reverseInvoice,
-    refundInvoice,
-} from './lib/smartpay';
-export * from './types';
