a1df24a29590ae55d99606d4d86f32ce51ca0ed0
Turanchoks <ipuncho@gmail.com>
2021-05-29 11:34:14 +0700

    chore: workshop example

--- examples/todo/src/pages/api/hook.ts
+++ examples/todo/src/pages/api/hook.ts
@@ -1,6 +1,145 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import {
+    createUserScenario,
+    createSystemScenario,
+    createSaluteRequest,
+    createSaluteResponse,
+    createScenarioWalker,
+    createMatchers,
+    SaluteRequest,
+    NLPRequest,
+    NLPResponse,
+} from '@salutejs/scenario';
+import { SaluteMemoryStorage } from '@salutejs/storage-adapter-memory';
 
-import { handleNlpRequest } from '../../scenario/scenario';
+import { AddNoteCommand, DeleteNoteCommand, DoneNoteCommand } from '../../types';
+
+const capitalize = (string: string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+const { action, regexp, selectItem } = createMatchers<SaluteRequest>();
+
+const userScenario = createUserScenario({
+    AddNote: {
+        match: regexp(/^(запиши|напомни|добавь запись) (?<note>.+)$/i),
+        handle: ({ req, res }) => {
+            const { note } = req.variables;
+            if (typeof note !== 'string') {
+                throw new Error('Wrong note type');
+            }
+
+            res.appendCommand<AddNoteCommand>({ type: 'add_note', payload: { note: capitalize(note) } });
+            res.appendSuggestions(['Запиши купить молоко', 'Добавь запись помыть машину']);
+            res.setPronounceText('Добавлено');
+            res.appendBubble('Добавлено');
+        },
+    },
+    DoneNote: {
+        match: regexp(/^(выполнила?|сделала?) (?<note>.+)$/i),
+        handle: ({ req, res }) => {
+            const { note } = req.variables;
+            const item = selectItem({ title: note })(req);
+            if (note && item?.id) {
+                res.appendCommand<DoneNoteCommand>({
+                    type: 'done_note',
+                    payload: { id: item.id },
+                });
+
+                res.setPronounceText('Умничка');
+                res.appendBubble('Умничка');
+            }
+        },
+    },
+    DoneNoteAction: {
+        match: action('done'),
+        handle: ({ req, res }) => {
+            const { note } = req.variables;
+            const item = selectItem({ title: note })(req);
+            if (note && item?.id) {
+                res.appendCommand<DoneNoteCommand>({
+                    type: 'done_note',
+                    payload: { id: item.id },
+                });
+
+                res.setPronounceText('Умничка');
+                res.appendBubble('Умничка');
+            }
+        },
+    },
+    DeleteNote: {
+        match: regexp(/^(удалить|удали) (?<note>.+)$/i),
+        handle: ({ req, res, session }) => {
+            const { note } = req.variables;
+            const item = selectItem({ title: note })(req);
+            if (note && item?.id) {
+                session.itemId = item.id;
+
+                res.setPronounceText('Вы уверены?');
+                res.appendBubble('Вы уверены?');
+                res.appendSuggestions(['продолжить', 'отменить']);
+            }
+        },
+        children: {
+            yes: {
+                match: regexp(/^(да|продолжить)$/i),
+                handle: ({ res, session }) => {
+                    const { itemId } = session;
+
+                    if (typeof itemId === 'string') {
+                        res.appendCommand<DeleteNoteCommand>({
+                            type: 'delete_note',
+                            payload: { id: itemId },
+                        });
+
+                        res.setPronounceText('Удалено');
+                        res.appendBubble('Удалено');
+                    }
+                },
+            },
+            no: {
+                match: regexp(/^(нет|отменить)$/i),
+                handle: ({ res }) => {
+                    res.setPronounceText('Удаление отменено');
+                    res.appendBubble('Удаление отменено');
+                },
+            },
+        },
+    },
+});
+
+// Системный сценарий
+const systemScenario = createSystemScenario({
+    RUN_APP: ({ res }) => {
+        res.appendSuggestions(['Запиши купить молоко', 'Добавь запись помыть машину']);
+        res.setPronounceText('начнем');
+        res.appendBubble('Начнем');
+    },
+    NO_MATCH: ({ res }) => {
+        res.setPronounceText('Я не понимаю');
+        res.appendBubble('Я не понимаю');
+    },
+});
+
+// Где сохраняется контекст
+const storage = new SaluteMemoryStorage();
+
+// Немного бойлерплейта
+const scenarioWalker = createScenarioWalker({
+    systemScenario,
+    userScenario,
+});
+
+const handleNlpRequest = async (request: NLPRequest): Promise<NLPResponse> => {
+    const req = createSaluteRequest(request);
+    const res = createSaluteResponse(request);
+
+    const sessionId = request.uuid.userId;
+    const session = await storage.resolve(sessionId);
+
+    await scenarioWalker({ req, res, session });
+    await storage.save({ id: sessionId, session });
+
+    return res.message;
+};
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
     response.status(200).json(await handleNlpRequest(request.body));
--- examples/todo/src/pages/index.tsx
+++ examples/todo/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useState, useRef, useEffect, FormEvent } from 'react';
+import { useReducer, useRef, useEffect, FormEvent } from 'react';
 import {
     AssistantCharacterType,
     AssistantAppState,
@@ -20,9 +20,11 @@ import {
     TextField,
     Checkbox,
 } from '@sberdevices/plasma-ui';
+import { createGlobalStyle } from 'styled-components';
+import { darkJoy, darkEva, darkSber } from '@sberdevices/plasma-tokens/themes';
+import { text, background, gradient } from '@sberdevices/plasma-tokens';
 
-import { GlobalStyles } from '../Components/GlobalStyles';
-import { Action, reducer } from '../store';
+import { AddNoteCommand, DoneNoteCommand, DeleteNoteCommand, Note } from '../types';
 
 if (process.browser) {
     // @ts-ignore
@@ -30,54 +32,156 @@ if (process.browser) {
 }
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+// eslint-disable-next-line
 const NEXT_PUBLIC_DEV_TOKEN = process.env.NEXT_PUBLIC_DEV_TOKEN;
+// eslint-disable-next-line
 const NEXT_PUBLIC_DEV_PHRASE = process.env.NEXT_PUBLIC_DEV_PHRASE;
+
+type Action =
+    | AddNoteCommand
+    | DoneNoteCommand
+    | DeleteNoteCommand
+    | {
+          type: 'Type note';
+          payload: string;
+      }
+    | {
+          type: 'Set character';
+          payload: AssistantCharacterType;
+      };
 
 interface TodoCommand extends AssistantSmartAppData {
     smart_app_data: Action;
 }
 
-const IndexPage = () => {
-    const [appState, dispatch] = useReducer(reducer, {
-        notes: [{ id: 'uinmh', title: 'купить хлеб', completed: false }],
-    });
+type State = {
+    notes: Array<Note>;
+    character: AssistantCharacterType;
+    note: string;
+};
 
-    const [character, setCharacter] = useState<AssistantCharacterType>('sber' as const);
-    const [note, setNote] = useState('');
+const reducer = (state: State, action: Action) => {
+    switch (action.type) {
+        case 'add_note':
+            return {
+                ...state,
+                notes: [
+                    ...state.notes,
+                    {
+                        id: Math.random().toString(36).substring(7),
+                        title: action.payload.note,
+                        completed: false,
+                    },
+                ],
+                note: '',
+            };
+
+        case 'done_note':
+            return {
+                ...state,
+                notes: state.notes.map((note) => (note.id === action.payload.id ? { ...note, completed: true } : note)),
+            };
+
+        case 'delete_note':
+            return {
+                ...state,
+                notes: state.notes.filter(({ id }) => id !== action.payload.id),
+            };
+
+        case 'Set character':
+            return {
+                ...state,
+                character: action.payload,
+            };
+
+        case 'Type note':
+            return {
+                ...state,
+                note: action.payload,
+            };
+
+        default:
+            throw new Error();
+    }
+};
+
+const themes = {
+    sber: createGlobalStyle(darkEva),
+    eva: createGlobalStyle(darkSber),
+    joy: createGlobalStyle(darkJoy),
+};
+
+const DocStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: rgba(255, 255, 255, 0); 
+    -webkit-tap-highlight-color: transparent; /* i.e. Nexus5/Chrome and Kindle Fire HD 7'' */
+  }
+  html {
+    font-size: 32px;
+    user-select: none;
+  }
+  body {
+    font-family: "SB Sans Text", sans-serif;
+    height: auto;
+    min-height: 100%;
+  }
+  body:before {
+    content: "";
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    color: ${text};
+    background: ${gradient};
+    background-color: ${background};
+    background-attachment: fixed;
+    background-size: 100vw 100vh;
+    z-index: -2;
+  }
+`;
+
+const IndexPage = () => {
+    const [{ note, character, notes }, dispatch] = useReducer(reducer, {
+        notes: [{ id: 'uinmh', title: 'купить хлеб', completed: false }],
+        character: 'sber' as const,
+        note: '',
+    });
 
     const assistantStateRef = useRef<AssistantAppState>({});
     const assistantRef = useRef<ReturnType<typeof createAssistant>>();
 
     useEffect(() => {
+        // Инициализируем ассистента
         const initializeAssistant = () => {
-            if (!IS_DEVELOPMENT) {
-                return createAssistant<TodoCommand>({
+            if (IS_DEVELOPMENT) {
+                return createSmartappDebugger({
+                    token: NEXT_PUBLIC_DEV_TOKEN,
+                    initPhrase: NEXT_PUBLIC_DEV_PHRASE,
                     getState: () => assistantStateRef.current,
                 });
             }
 
-            if (!NEXT_PUBLIC_DEV_TOKEN || !NEXT_PUBLIC_DEV_PHRASE) {
-                throw new Error('');
-            }
-
-            return createSmartappDebugger({
-                token: NEXT_PUBLIC_DEV_TOKEN,
-                initPhrase: NEXT_PUBLIC_DEV_PHRASE,
+            return createAssistant<TodoCommand>({
                 getState: () => assistantStateRef.current,
             });
         };
 
         const assistant = initializeAssistant();
 
+        // Подписываемся на данные
         assistant.on('data', (command: AssistantClientCustomizedCommand<TodoCommand>) => {
             let navigation: AssistantNavigationCommand['navigation'];
             switch (command.type) {
                 case 'character':
-                    setCharacter(command.character.id);
-                    // 'sber' | 'eva' | 'joy';
+                    dispatch({
+                        type: 'Set character',
+                        payload: command.character.id,
+                    });
                     break;
                 case 'navigation':
-                    navigation = (command as AssistantNavigationCommand).navigation;
+                    navigation = command.navigation;
                     break;
                 case 'smart_app_data':
                     dispatch(command.smart_app_data);
@@ -106,22 +210,25 @@ const IndexPage = () => {
     useEffect(() => {
         assistantStateRef.current = {
             item_selector: {
-                items: appState.notes.map(({ id, title }, index) => ({
+                items: notes.map(({ id, title }, index) => ({
                     number: index + 1,
                     id,
                     title,
                 })),
             },
         };
-    }, [appState]);
+    }, [notes]);
 
     const doneNote = (title: string) => {
         assistantRef.current?.sendAction({ type: 'done', payload: { note: title } });
     };
 
+    const Theme = themes[character];
+
     return (
         <DeviceThemeProvider>
-            <GlobalStyles character={character} />
+            <Theme />
+            <DocStyles />
             <Container style={{ margin: '5rem 0 7rem' }}>
                 <Row>
                     <Col size={12} sizeXL={6} offsetXL={3}>
@@ -129,19 +236,23 @@ const IndexPage = () => {
                             onSubmit={(e: FormEvent<HTMLFormElement>) => {
                                 e.preventDefault();
                                 dispatch({ type: 'add_note', payload: { note } });
-                                setNote('');
                             }}
                         >
                             <TextField
                                 label="Add Note"
                                 value={note}
-                                onChange={({ target: { value } }) => setNote(value)}
+                                onChange={({ target: { value } }) => {
+                                    dispatch({
+                                        type: 'Type note',
+                                        payload: value,
+                                    });
+                                }}
                             />
                         </form>
                     </Col>
                 </Row>
                 <Row style={{ marginTop: '2rem' }}>
-                    {appState.notes.map((n, i) => (
+                    {notes.map((n, i) => (
                         <Col key={i} size={12} sizeXL={6} offsetXL={3} style={{ marginBottom: '1rem' }}>
                             <Card>
                                 <CardContent compact>
